5d9647b8db45c9a45f67dd137b3153c904855717
JacobButcher-Github <122327437+JacobButcher-Github@users.noreply.github.com>
2023-01-19 16:53:12 -0600

    Add elements to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,19 @@ export default function Home() {
               <Link href="/lists/ingredients.js">Ingredients</Link>
             </div>
           </div>
-        <div id='search_block'>
+          <div class="logo_holder">PLACEHOLDER</div>
+        <div id='search_block_front' class='placeholder'>
+          <table class="meal_types">
+            <tbody>
+            <tr>
+              <td><div id="breakfast_button" class="cs 1">Breakfast</div></td>
+              <td><div id="lunch_button" class="cs 2">Lunch</div></td>
+              <td><div id="dinner_button" class="cs 3">Dinner</div></td>
+              <td><div id="snack_button" class="cs 4">Snack</div></td>
+              <td><div id="desert_button" class="cs 5">Desert</div></td>
+            </tr>
+            </tbody>
+          </table>
           <input type="text" placeholder="Ingredient.."></input>
           <select name='Amount'>
             <option value="very_little">Very Little</option>
