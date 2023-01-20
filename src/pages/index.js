d437d2499f7abc44a08d66d153c52943e5008802
JacobButcher-Github <122327437+JacobButcher-Github@users.noreply.github.com>
2023-01-19 22:10:42 -0600

    In Progress: Add functionality to Add Ingredient Btn

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,26 @@
-import Head from 'next/head'
+import Head from 'next/head';
 import Link from 'next/link';
-import { Inter } from '@next/font/google'
+import { createRoot } from 'react-dom/client';
+import { Inter } from '@next/font/google';
 
 const inter = Inter({ subsets: ['latin'] })
+
+function Add_Search(){
+  const search = document.getElementById("search");
+  const root = createRoot(search);
+  root.render(
+  <>
+  <input type="text" placeholder="Ingredient.."></input>
+  <select name='Amount'>
+    <option value="very_little">Very Little</option>
+    <option value="little">Little</option>
+    <option value="medium">Medium</option>
+    <option value="large">Large</option>
+    <option value="very_large">Very Large</option>
+  </select>
+  </>
+);
+}
 
 export default function Home() {
   return (
@@ -33,18 +51,21 @@ export default function Home() {
               <td><div id="lunch_button" class="cs 2">Lunch</div></td>
               <td><div id="dinner_button" class="cs 3">Dinner</div></td>
               <td><div id="snack_button" class="cs 4">Snack</div></td>
-              <td><div id="desert_button" class="cs 5">Desert</div></td>
+              <td><div id="dessert_button" class="cs 5">Dessert</div></td>
             </tr>
             </tbody>
           </table>
-          <input type="text" placeholder="Ingredient.."></input>
-          <select name='Amount'>
-            <option value="very_little">Very Little</option>
-            <option value="little">Little</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
-            <option value="very_large">Very Large</option>
-          </select>
+          <div id="search">
+            <input type="text" placeholder="Ingredient.."></input>
+            <select name='Amount'>
+              <option value="very_little">Very Little</option>
+              <option value="little">Little</option>
+              <option value="medium">Medium</option>
+              <option value="large">Large</option>
+              <option value="very_large">Very Large</option>
+            </select>
+          </div>
+          <button type="button" id="add_igredient" onClick={Add_Search}>Add Ingredient</button>
         </div>
         </main>
     </>
